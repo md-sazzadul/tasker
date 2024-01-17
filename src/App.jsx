bdcef68f4f56e82baa4e99c5a5b259c65c329b7b
sazzadul.islam <sazzadul.islam@stein.de>
2024-01-17 10:36:44 +0100

    added footer and section section

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
+import Footer from "./components/Footer/Footer";
 import Header from "./components/Header/Header";
+import HeroSection from "./components/HeroSection/HeroSection";
 
 function App() {
   return (
     <>
       <Header></Header>
+      <HeroSection></HeroSection>
+      <Footer></Footer>
     </>
   );
 }

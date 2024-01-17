4790f5a7c7fce3de1cb8d265a1600b410586d283
sazzadul.islam <sazzadul.islam@stein.de>
2024-01-17 11:25:37 +0100

    added dynamic tasks in task list

--- src/components/TaskBoard/TaskBoard.jsx
+++ src/components/TaskBoard/TaskBoard.jsx
@@ -1,8 +1,20 @@
+import { useState } from "react";
 import SearchTask from "./SearchTask";
 import TaskActions from "./TaskActions";
 import TaskList from "./TaskList";
 
 const TaskBoard = () => {
+  const defaultTask = {
+    id: crypto.randomUUID(),
+    title: "Learn React",
+    description:
+      "I want to Learn React such that I can treat it like my slave and make it do whatever I want to do.",
+    tags: ["web", "react", "js"],
+    priority: "High",
+    isFavorite: true,
+  };
+  const [tasks, setTasks] = useState([defaultTask]);
+
   return (
     <section className="mb-20" id="tasks">
       <div className="container">
@@ -11,7 +23,7 @@ const TaskBoard = () => {
         </div>
         <div className="rounded-xl border border-[rgba(206,206,206,0.12)] bg-[#1D212B] px-6 py-8 md:px-9 md:py-16">
           <TaskActions></TaskActions>
-          <TaskList></TaskList>
+          <TaskList tasks={tasks}></TaskList>
         </div>
       </div>
     </section>

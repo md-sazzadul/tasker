df2dd5a4556969d0ff2abd75c5b529695ca19321
sazzadul.islam <sazzadul.islam@stein.de>
2024-01-17 10:56:17 +0100

    added taskBoard and searchTask

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import Footer from "./components/Footer/Footer";
 import Header from "./components/Header/Header";
 import HeroSection from "./components/HeroSection/HeroSection";
+import TaskBoard from "./components/TaskBoard/TaskBoard";
 
 function App() {
   return (
     <>
       <Header></Header>
-      <HeroSection></HeroSection>
+      <div className="flex flex-col justify-center items-center">
+        <HeroSection></HeroSection>
+        <TaskBoard></TaskBoard>
+      </div>
       <Footer></Footer>
     </>
   );
